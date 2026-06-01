a04d532c5b4b5eccc8536d760ffd725c016ed880
Khurram Ali <khurramcali@gmail.com>
2026-06-01 00:50:35 -0400

    fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   },
   description: 'Track every concert you\'ve ever been to.',
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_APP_URL ??
+    process.env.BASE_URL ??
       (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'),
   ),
   openGraph: {
